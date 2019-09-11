ad6fe410fca1d751853a1c0cb7a725ef3de6cd91
jasvirkaur <jasvir.kaur1224@gmail.com>
2019-09-10 21:28:49 -0400

    Updated One

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-
 		System.setProperty("webdriver.chrome.driver", "/Users/jasvirkaur/Desktop/chromedriver");
 		WebDriver driver =  new ChromeDriver();
+
 		
 		driver.get("http://www.mcdonalds.ca/");
 		
+		WebElement titlemsg = driver.findElement(By.cssSelector("h2.click-before-outline"));
+   titlemsg.sendKeys("Subscribe to My McD's®");
+		
 		WebElement  firstName = driver.findElement(By.id("firstname2"));
 		firstName.sendKeys("Jasvir");
-		//firstName.getText();
 		
 		
 		WebElement  email = driver.findElement(By.id("email2"));
 	email.sendKeys("jasvir.kaur@gmail.com");
 
-	//email.getText();
 		
 WebElement  postalCode = driver.findElement(By.id("postalcode2"));
   postalCode.sendKeys("L6R");
 
-	//	postalCode.getText();
 		
 		WebElement subscribeButton = driver.findElement(By.id("g-recaptcha-btn-2"));
 		subscribeButton.click();
--- test/JunitTests.java
+++ test/JunitTests.java
@@ -16,39 +16,81 @@ public class JunitTests {
 	public void setUp() throws Exception {
 		
 		System.setProperty("webdriver.chrome.driver", "/Users/jasvirkaur/Desktop/chromedriver");
-		WebDriver driver =  new ChromeDriver();
-		
-		driver.get("www.mcdonalds.ca" );
+		driver = new ChromeDriver();
+
+		driver.get("http://www.mcdonalds.ca/");
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		
-	Thread.sleep(2000);
+	Thread.sleep(5000);
 	driver.close();
 	}
+	
+/*	@Test
+	public void testCase1() {
+		
+	WebElement titlemsg = driver.findElement(By.cssSelector("h2.click-before-outline"));
+	String actualTitle = titlemsg.getText();
+					System.out.println(actualTitle);
+	assertEquals("Subscribe to My McD’s®", actualTitle);
+		
+		}*/
+	
+	
 
 	@Test
 	public void testCase2() {
 		
 		WebElement  firstName = driver.findElement(By.id("firstname2"));
+		 firstName.sendKeys("Jasvir");
 		 String actualFirstName =firstName.getText();
-		 assertEquals(actualFirstName ,firstName );
 		
-		
+			
 		WebElement  email = driver.findElement(By.id("email2"));
+		email.sendKeys("jasvir.kaur@gmail.com");
 		String actualemail = email.getText();
-		 assertEquals(actualemail ,email );
-
 		
-
 		WebElement  postalCode = driver.findElement(By.id("postalcode2"));
-		postalCode.getText();
+		postalCode.sendKeys("L6R");
+		String actualCode = postalCode.getText();
+		
+		 
+		assertEquals("Jasvir",actualFirstName);
+		 assertEquals("jasvir.kaur@gmail.com",actualemail );
+
+		assertEquals("L6R",actualCode );
+		
 		
 		WebElement subscribeButton = driver.findElement(By.id("g-recaptcha-btn-2"));
 		subscribeButton.click();
 		
 		
-	}
+		
+		}
+	
+	/*@Test
+	public void testCase3() 
+	{
+		
+		WebElement  firstName = driver.findElement(By.id("firstname2"));
+		 String actualFirstName = null;
+		 assertEquals("Jasvir" , actualFirstName );
+			
+		WebElement  email = driver.findElement(By.id("email2"));
+		String actualemail = null;
+		 assertEquals("jasvir.kaur@gmail.com" , actualemail );
+
+		WebElement  postalCode = driver.findElement(By.id("postalcode2"));
+		String actualCode = null;
+		 assertEquals("L6R" , actualCode );
+
+		
+		WebElement subscribeButton = driver.findElement(By.id("g-recaptcha-btn-2"));
+		subscribeButton.click();
+		}*/
+	
+	
 
 }
